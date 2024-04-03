89a71a1b9396bf93ec4820f08de0c73c56256987
olugbemiopedepo <olugbemiopedepo@gmail.com>
2024-04-03 23:45:54 +0000

    update to app

---
+++ .idx/dev.nix
@@ -0,0 +1,65 @@
+# To learn more about how to use Nix to configure your environment
+
+# see: https://developers.google.com/idx/guides/customize-idx-env
+
+{ pkgs, ... }: {
+
+  # Which nixpkgs channel to use.
+
+  channel = "stable-23.11"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+
+  packages = [
+    pkgs.unzip
+
+  ];
+
+  # Sets environment variables in the workspace
+
+  env = { };
+
+  idx = {
+
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+
+    extensions = [
+
+      # "vscodevim.vim"
+
+    ];
+
+    # Enable previews and customize configuration
+
+    previews = {
+
+      enable = true;
+
+      previews = [
+
+        {
+
+          command = [
+            "reflex"
+            "run"
+            "demo"
+            "--"
+            "--port"
+            "$PORT"
+            "--host"
+            "0.0.0.0"
+            "--disable-host-check"
+          ];
+
+          id = "web";
+
+          manager = "web";
+
+        }
+      ];
+
+    };
+
+  };
+
+}
