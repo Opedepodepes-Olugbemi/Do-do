ff1747e7dd24476842892b23c3252c80adcf4a6d
olugbemiopedepo <olugbemiopedepo@gmail.com>
2024-04-04 10:01:16 +0000

    update

--- Do_do/Do_do.py
+++ Do_do/Do_do.py
@@ -1,11 +1,6 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
-
 from rxconfig import config
 
 import reflex as rx
-
-docs_url = "https://reflex.dev/docs/getting-started/introduction/"
-filename = f"{config.app_name}/{config.app_name}.py"
 
 
 class State(rx.State):
